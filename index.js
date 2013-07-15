4bf179e1c24124e6f52e090883f3497d0c0eacef
Julian Gruber <julian@juliangruber.com>
2013-07-15 08:00:26 +0200

    refactor

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// todo: use pull streams
 var through = require('through');
 var geohash = require('geohash').GeoHash.encodeGeoHash;
 var shutup = require('shutup');
@@ -10,71 +9,57 @@ function Places (db) {
   this.db = db;
 }
 
-/**
- * @param {Object} data
- * @param {Number} lat
- * @param {Number} lon
- * @param {Function=} fn
- */
-
 Places.prototype.add = function (data, lat, lon, fn) {
   var hash = geohash(lat, lon);
   var rand = Math.random().toString(16).slice(2);
-  // todo: make new batch api usable
-  // var batch = this.db.batch();
-  var ops = [];
+  // todo: use monotonic-timestamp
 
-  for (var i = 0; i < hash.length; i++) {
-    ops.push({
+  this.db.batch(hash.split('').map(function (_, i) {
+    return {
       type: 'put',
       key: hash.substr(0, i+1) + '!' + rand,
+      valueEncoding: 'json',
       value: {
         hash: hash + rand,
         data: data
       }
-    });
-  }
-
-  this.db.batch(ops, { valueEncoding : 'json' }, fn);
+    };
+  }), fn);
 };
-
-/**
- * @param {Number} lat
- * @param {Number} lon
- * @param {Object=} opts
- */
 
 Places.prototype.createReadStream = function (lat, lon, opts) {
   if (!opts) opts = {};
+
   var db = this.db;
   var found = [];
-  var limit = typeof opts.limit != 'undefined'
-    ? opts.limit
-    : Infinity;
+  var limit = typeof opts.limit != 'undefined'? opts.limit : Infinity;
   var outer = shutup(through());
+  // todo: use pull-streams
 
-  (function read (hash) {
-    var rs = db.createValueStream({ start: hash, valueEncoding: 'json' });
+  function read (hash) {
+    var vs = db.createValueStream({ start: hash, valueEncoding: 'json' });
     var inner = through(write, end);
 
     function write (val) {
-      if (found.indexOf(val.hash) == -1) {
-        found.push(val.hash);
-        inner.queue(val.data);
-        if (found.length == limit) rs.destroy();
-      }
+      if (found.indexOf(val.hash) != -1) return;
+      found.push(val.hash);
+      inner.queue(val.data);
+      if (found.length == limit) vs.destroy();
     }
     function end () {
-      if (hash.length > 0 && found.length < limit) {
-        read(hash.substr(0, hash.length - 1));
-      } else {
-        outer.end();
-      }
+      hash.length > 0 && found.length < limit
+        ? read(hash.substr(0, hash.length - 1))
+        : outer.end();
     }
 
-    rs.pipe(inner).pipe(outer, { end: false });
-
-  })(geohash(lat, lon));
+    vs.pipe(inner).pipe(outer, { end: false });
+  }
+  
+  process.nextTick(function () {
+    read(geohash(lat, lon));
+    // todo: or use setImmediate
+  });
 
   return outer;
 };
+
